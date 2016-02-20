24552a234e405989c1a14c8d12c5d0dc436e34db
Kenneth Soh <kennethsohyq@gmail.com>
2016-02-20 19:10:45 +0800

    Added a basic Main Menu (temporarily using placeholders)

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/RecyclerAdapters/StringRecyclerAdapter.java
@@ -0,0 +1,72 @@
+package com.itachi1706.cheesecakeutilities.RecyclerAdapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.itachi1706.cheesecakeutilities.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by itachi1706 on 2/20/2016.
+ * For com.itachi1706.cheesecakeutilities.RecyclerAdapters in Cheesecake Utilities.
+ */
+public class StringRecyclerAdapter extends RecyclerView.Adapter<StringRecyclerAdapter.StringViewHolder> {
+    private List<String> stringList;
+
+    public StringRecyclerAdapter(List<String> strings)
+    {
+        this.stringList = strings;
+    }
+
+    public StringRecyclerAdapter(String[] strings)
+    {
+        this.stringList = Arrays.asList(strings);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return stringList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(StringViewHolder stringViewHolder, int i)
+    {
+        String s  = stringList.get(i);
+        stringViewHolder.title.setText(s);
+    }
+
+    @Override
+    public StringViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.recyclerview_default_simple_list_item_1, viewGroup, false);
+
+        return new StringViewHolder(itemView);
+    }
+
+
+    public class StringViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        protected TextView title;
+
+        public StringViewHolder(View v)
+        {
+            super(v);
+            title = (TextView) v.findViewById(R.id.text1);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(v.getContext(), title.getText(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
