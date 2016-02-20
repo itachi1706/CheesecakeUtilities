bfb77fc2204a8dbfce1ddda7950e1a7a86aed1ff
Kenneth Soh <kennethsohyq@gmail.com>
2016-02-20 19:10:45 +0800

    Added a basic Main Menu (temporarily using placeholders)

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/MainMenuActivity.java
@@ -0,0 +1,40 @@
+package com.itachi1706.cheesecakeutilities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.itachi1706.cheesecakeutilities.RecyclerAdapters.MainMenuAdapter;
+import com.itachi1706.cheesecakeutilities.RecyclerAdapters.StringRecyclerAdapter;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.main_menu_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        // Set up layout
+        String[] menuitems = getResources().getStringArray(R.array.mainmenu);
+        MainMenuAdapter adapter = new MainMenuAdapter(menuitems);
+        recyclerView.setAdapter(adapter);
+    }
+
+}
---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/RecyclerAdapters/MainMenuAdapter.java
@@ -0,0 +1,75 @@
+package com.itachi1706.cheesecakeutilities.RecyclerAdapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.itachi1706.cheesecakeutilities.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by itachi1706 on 2/20/2016.
+ * For com.itachi1706.cheesecakeutilities.RecyclerAdapters in Cheesecake Utilities.
+ */
+public class MainMenuAdapter extends RecyclerView.Adapter<MainMenuAdapter.MainMenuHolder> {
+    private List<String> stringList;
+
+    public MainMenuAdapter(List<String> strings)
+    {
+        this.stringList = strings;
+    }
+
+    public MainMenuAdapter(String[] strings)
+    {
+        this.stringList = Arrays.asList(strings);
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return stringList.size();
+    }
+
+    @Override
+    public void onBindViewHolder(MainMenuHolder stringViewHolder, int i)
+    {
+        String s  = stringList.get(i);
+        stringViewHolder.title.setText(s);
+    }
+
+    @Override
+    public MainMenuHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.recyclerview_default_simple_list_item_1, viewGroup, false);
+
+        return new MainMenuHolder(itemView);
+    }
+
+
+    public class MainMenuHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        protected TextView title;
+
+        public MainMenuHolder(View v)
+        {
+            super(v);
+            title = (TextView) v.findViewById(R.id.text1);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (title.getText().toString()) {
+                case "Hello World!": Toast.makeText(v.getContext(), title.getText() + " (0)", Toast.LENGTH_SHORT).show(); break;
+                case "Bye World :(": Toast.makeText(v.getContext(), title.getText() + " (1)", Toast.LENGTH_SHORT).show(); break;
+            }
+        }
+
+    }
+}
