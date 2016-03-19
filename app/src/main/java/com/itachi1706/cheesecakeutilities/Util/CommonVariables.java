2c5181686f13ab715f482a9274521c85b705c604
itachi1706 <kennethsohyq@gmail.com>
2016-03-19 19:03:53 +0800

    Converted Updater to Modules

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Util/CommonVariables.java
@@ -0,0 +1,10 @@
+package com.itachi1706.cheesecakeutilities.Util;
+
+/**
+ * Created by Kenneth on 3/19/2016.
+ * for com.itachi1706.cheesecakeutilities.Util in CheesecakeUtilities
+ */
+public class CommonVariables {
+
+    public static final String BASE_SERVER_URL = "http://api.itachi1706.com/api/appupdatechecker.php?action=androidretrievedata&packagename=";
+}
