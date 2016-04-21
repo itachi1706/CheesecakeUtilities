5c9b6aeed61e820c2a01b3bf7ca3cc4325ed5732
itachi1706 <kennethsohyq@gmail.com>
2016-04-21 22:14:27 +0800

    Added Fanfiction Compacting Utility Shell - Able to request storage permission - Able to calculate folder size

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Util/CommonMethods.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Util/CommonMethods.java
@@ -3,6 +3,8 @@ package com.itachi1706.cheesecakeutilities.Util;
 import android.app.Activity;
 import android.app.AlertDialog;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by Kenneth on 3/19/2016.
  * for com.itachi1706.cheesecakeutilities.Util in CheesecakeUtilities
@@ -15,6 +17,13 @@ public class CommonMethods {
                         " may or may not be present in the release application.\n\nBugs and Crashes " +
                         "are to be expected for the utility")
                 .setPositiveButton(android.R.string.ok, null).show();
+    }
+
+    public static String readableFileSize(long size) {
+        if(size <= 0) return "0";
+        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+        int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
 
 }
