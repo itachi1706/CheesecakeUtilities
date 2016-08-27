648a52f99c3fcdf8c2489cf500000c1c6ae1fab5
itachi1706 <kennethsohyq@gmail.com>
2016-08-27 22:30:51 +0800

    Roughly completed the coding of the settings portion of the calculator - Added icons - Enabled vector drawables in app - Upgraded dependencies

--- app/src/main/java/com/itachi1706/cheesecakeutilities/ORDActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/ORDActivity.java
@@ -3,24 +3,41 @@ package com.itachi1706.cheesecakeutilities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class ORDActivity extends AppCompatActivity {
+public class ORDActivity extends BaseActivity {
 
-    Button settings;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ord);
 
-        settings = (Button) findViewById(R.id.button);
-        settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), ORDSettingsActivity.class));
-            }
-        });
+        recyclerView = (RecyclerView) findViewById(R.id.ord_recycler_view);
+    }
+
+    @Override
+    String getHelpDescription() {
+        return "ORD Calculator (WIP)";
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.modules_ord, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.modify_settings: startActivity(new Intent(getApplicationContext(), ORDSettingsActivity.class)); return true;
+            default: return super.onOptionsItemSelected(item);
+        }
     }
 }
