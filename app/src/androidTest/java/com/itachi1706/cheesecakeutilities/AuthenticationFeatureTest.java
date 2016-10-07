3fb685e4d88143e746de0555595e9aa8f14dc8b1
itachi1706 <kennethsohyq@gmail.com>
2016-10-07 22:40:51 +0800

    Added a espresso test on testing authentication feature

---
+++ app/src/androidTest/java/com/itachi1706/cheesecakeutilities/AuthenticationFeatureTest.java
@@ -0,0 +1,212 @@
+package com.itachi1706.cheesecakeutilities;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.filters.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.view.WindowManager;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AuthenticationFeatureTest {
+
+    @Rule
+    public ActivityTestRule<MainMenuActivity> mActivityTestRule = new ActivityTestRule<>(MainMenuActivity.class);
+
+    @Before
+    public void unlockScreen() {
+        final MainMenuActivity activity = mActivityTestRule.getActivity();
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
+
+    @Test
+    public void authenticationFeatureTest() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.title), withText("Settings"), isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withId(android.R.id.list_container))),
+                        2),
+                        isDisplayed()));
+        linearLayout.perform(click());
+
+        ViewInteraction editText = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText.perform(click());
+
+        ViewInteraction editText2 = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText2.perform(replaceText("qwerty"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        withParent(allOf(withId(R.id.buttonPanel),
+                                withParent(withId(R.id.parentPanel)))),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(android.R.id.title), withText("Change/Remove Password"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        1),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Change/Remove Password")));
+
+        ViewInteraction linearLayout2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.list),
+                                withParent(withId(android.R.id.list_container))),
+                        2),
+                        isDisplayed()));
+        linearLayout2.perform(click());
+
+        ViewInteraction editText3 = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText3.perform(click());
+
+        ViewInteraction editText4 = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText4.perform(replaceText("qwert"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        withParent(allOf(withId(R.id.buttonPanel),
+                                withParent(withId(R.id.parentPanel)))),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+
+        ViewInteraction editText5 = onView(
+                allOf(childAtPosition(
+                                allOf(withId(R.id.custom),
+                                        childAtPosition(
+                                                withId(R.id.customPanel),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        editText5.check(matches(hasErrorText("Invalid Password")));
+
+        ViewInteraction editText6 = onView(
+                allOf(withClassName(is("android.widget.EditText")),
+                        withParent(allOf(withId(R.id.custom),
+                                withParent(withId(R.id.customPanel)))),
+                        isDisplayed()));
+        editText6.perform(clearText(), typeText("qwerty"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        withParent(allOf(withId(R.id.buttonPanel),
+                                withParent(withId(R.id.parentPanel)))),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.alertTitle),
+                        isDisplayed()));
+        textView2.check(matches(withText("Set new Password")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(android.R.id.message), withText("Set a new app password or leave it blank to have no password"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.scrollView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView3.check(matches(withText("Set a new app password or leave it blank to have no password")));
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(android.R.id.button1), withText("OK"),
+                        withParent(allOf(withId(R.id.buttonPanel),
+                                withParent(withId(R.id.parentPanel)))),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(android.R.id.summary), withText("Unprotected"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        1),
+                                1),
+                        isDisplayed()));
+        textView4.check(matches(withText("Unprotected")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
