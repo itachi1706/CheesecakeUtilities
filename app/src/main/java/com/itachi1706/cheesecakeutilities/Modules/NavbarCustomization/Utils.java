07f0f0c467d0e428f72c69646bb17d71d767bde8
itachi1706 <kennethsohyq@gmail.com>
2016-10-21 17:41:58 +0800

    Tweaked some customization options and navbar view layout

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/NavbarCustomization/Utils.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/NavbarCustomization/Utils.java
@@ -5,4 +5,7 @@ import android.os.Build;
 class Utils {
 
     static final boolean IS_AT_LEAST_MARSHMALLOW = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+    static final String NAVBAR_SHOW_CLOCK = "navbar_clock";
+    static final String NAVBAR_SHOW_IMAGE = "navbar_image";
+    static final String NAVBAR_SHOW_APPNAME = "navbar_appname";
 }
