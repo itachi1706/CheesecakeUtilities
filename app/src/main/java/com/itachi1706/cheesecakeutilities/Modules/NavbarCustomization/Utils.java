7df9ee926a4d4835fbf57b6673a2f8ac23c8fa6c
itachi1706 <kennethsohyq@gmail.com>
2016-10-21 12:52:54 +0800

    Added shell of navbar customization utility. Time to start tweaking it Implementing #39

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/NavbarCustomization/Utils.java
@@ -0,0 +1,8 @@
+package com.itachi1706.cheesecakeutilities.Modules.NavbarCustomization;
+
+import android.os.Build;
+
+class Utils {
+
+    static final boolean IS_AT_LEAST_MARSHMALLOW = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+}
