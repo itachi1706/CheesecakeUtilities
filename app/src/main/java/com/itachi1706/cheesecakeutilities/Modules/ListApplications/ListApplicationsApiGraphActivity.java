f66d94c97691046e948c19c2a5e48eca3a409fc0
itachi1706 <kennethsohyq@gmail.com>
2016-12-03 22:28:45 +0800

    Finish doing the graph creation Finished implementing and closes #8

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/ListApplications/ListApplicationsApiGraphActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/ListApplications/ListApplicationsApiGraphActivity.java
@@ -2,29 +2,38 @@ package com.itachi1706.cheesecakeutilities.Modules.ListApplications;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.util.ArrayMap;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.itachi1706.cheesecakeutilities.R;
+import com.itachi1706.cheesecakeutilities.Util.CommonVariables;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static com.github.mikephil.charting.utils.ColorTemplate.rgb;
+public class ListApplicationsApiGraphActivity extends AppCompatActivity implements OnChartValueSelectedListener {
 
-public class ListApplicationsApiGraphActivity extends AppCompatActivity {
+    private RelativeLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_applications_api_graph);
+        if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (!getIntent().hasExtra("appCount")) {
             Toast.makeText(this, "Cannot plot graph. Please wait for the app list to finish loading first!", Toast.LENGTH_LONG).show();
@@ -47,18 +56,55 @@ public class ListApplicationsApiGraphActivity extends AppCompatActivity {
         }
 
         PieChart chart = (PieChart) findViewById(R.id.chart);
+        layout = (RelativeLayout) findViewById(R.id.activity_list_applications_api_graph);
         List<PieEntry> entries = new ArrayList<>();
         for (Map.Entry<Integer, Integer> object : appCountMap.entrySet()) {
             entries.add(new PieEntry((float) object.getValue(), "API " + object.getKey().toString()));
         }
         PieDataSet dataSet = new PieDataSet(entries, "API Version");
-        dataSet.setColors(colors, this);
-        //dataSet.setColors(android.R.color.holo_red_dark, android.R.color.holo_blue_bright);
-        //dataSet.setValueTextColor(Color.BLACK);
+        dataSet.setColors(CommonVariables.MATERIAL_COLORS);
+        chart.setNoDataText("No Applications on Device");
+        chart.setHoleRadius(20f);
+        Description desc = new Description();
+        desc.setText("");
+        chart.setDescription(desc);
+        chart.setUsePercentValues(true);
+        dataSet.setValueTextSize(15f);
+        dataSet.setValueTextColor(Color.WHITE);
+        dataSet.setValueFormatter(new PercentFormatter());
+        chart.setTransparentCircleRadius(20f);
+        Legend legend = chart.getLegend();
+        legend.setWordWrapEnabled(true);
+        legend.setTextSize(15f);
         PieData data = new PieData(dataSet);
         chart.setData(data);
+        chart.setOnChartValueSelectedListener(this);
         chart.invalidate();
     }
 
-    int[] colors = {rgb("#F44336")};
+    Snackbar snackbar = null;
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+        if (!(e instanceof PieEntry)) return;
+        if (snackbar != null && snackbar.isShownOrQueued()) {
+            snackbar.dismiss();
+            snackbar = null;
+        }
+
+        PieEntry pieEntry = (PieEntry) e;
+        String api = pieEntry.getLabel();
+        float count = pieEntry.getValue();
+
+        snackbar = Snackbar.make(layout, api + "\nApp Count: " + count, Snackbar.LENGTH_INDEFINITE);
+        snackbar.show();
+    }
+
+    @Override
+    public void onNothingSelected() {
+        if (snackbar != null && snackbar.isShownOrQueued())
+            snackbar.dismiss();
+
+        snackbar = null;
+    }
 }
