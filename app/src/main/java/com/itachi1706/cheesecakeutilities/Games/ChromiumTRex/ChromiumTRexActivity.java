a0f6ac2155c5583be97eeb0dd15db94762298d25
itachi1706 <kennethsohyq@gmail.com>
2017-01-20 21:17:19 +0800

    Added the Chromium T-Rex game Implements and Closes #74

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Games/ChromiumTRex/ChromiumTRexActivity.java
@@ -0,0 +1,33 @@
+package com.itachi1706.cheesecakeutilities.Games.ChromiumTRex;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+import com.itachi1706.cheesecakeutilities.BaseActivity;
+import com.itachi1706.cheesecakeutilities.R;
+
+public class ChromiumTRexActivity extends BaseActivity {
+
+    WebView webView;
+
+    @Override
+    public String getHelpDescription() {
+        return "This is the T-Rex game from the Chromium Browser that appears when you are offline!" +
+                "\n\nCopyright © 2014 The Chromium Authors. All rights reserved.";
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chromium_trex);
+
+        webView = (WebView) findViewById(R.id.webview);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        //webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/chromiumtrex/index.html");
+    }
+}
