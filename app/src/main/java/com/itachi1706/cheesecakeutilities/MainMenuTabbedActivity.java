df4f7e3f00312199e5c19368f9a89f329537dd9b
itachi1706 <kennethsohyq@gmail.com>
2017-01-31 17:43:25 +0800

    Created a new tabbed version of the main menu

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/MainMenuTabbedActivity.java
@@ -0,0 +1,103 @@
+package com.itachi1706.cheesecakeutilities;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+import com.itachi1706.appupdater.AppUpdateInitializer;
+import com.itachi1706.cheesecakeutilities.Features.FingerprintAuth.AuthenticationActivity;
+import com.itachi1706.cheesecakeutilities.Fragments.GamesFragment;
+import com.itachi1706.cheesecakeutilities.Fragments.UtilityFragment;
+import com.itachi1706.cheesecakeutilities.Util.CommonMethods;
+import com.itachi1706.cheesecakeutilities.Util.CommonVariables;
+
+import io.fabric.sdk.android.Fabric;
+
+public class MainMenuTabbedActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ViewPager pager;
+    TabLayout tabLayout;
+    SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Error Handling
+        Crashlytics crashlyticsKit = new Crashlytics.Builder().core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()).build();
+        Fabric.with(this, crashlyticsKit);
+
+        setContentView(R.layout.activity_main_menu_tabbed);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+
+        pager = (ViewPager) findViewById(R.id.main_viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.main_tablayout);
+
+        setupViewPager(pager);
+        tabLayout.setupWithViewPager(pager);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+        new AppUpdateInitializer(this, sp, R.mipmap.ic_launcher, CommonVariables.BASE_SERVER_URL).checkForUpdate(true);
+
+        // Do Authentication
+        boolean authagain = !this.getIntent().hasExtra("authagain") || this.getIntent().getExtras().getBoolean("authagain");
+        if (!authagain) return;
+        if (CommonMethods.isGlobalLocked(sp)) startActivityForResult(new Intent(this, AuthenticationActivity.class), REQUEST_AUTH);
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFrag(new UtilityFragment(), "Utilities");
+        adapter.addFrag(new GamesFragment(), "Games");
+
+        viewPager.setAdapter(adapter);
+    }
+
+    private final int REQUEST_AUTH = 2;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_AUTH) {
+            if (resultCode == RESULT_CANCELED) {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                startActivity(new Intent(this, GeneralSettingsActivity.class));
+                return true;
+            case R.id.exit:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
