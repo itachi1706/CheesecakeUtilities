373be49cc5eeaa8dc79ae9215665685f6a679c57
itachi1706 <kennethsohyq@gmail.com>
2017-02-25 22:21:44 +0800

    Implemented a empty activity to launch the companion module app for tiles
    Will invoke the play store if its not installed on the system
    It shall also be hidden and will enable when it actually releases on the Play Store

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/LaunchTileSettingsActivity.java
@@ -0,0 +1,38 @@
+package com.itachi1706.cheesecakeutilities;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchTileSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Check if tiles companion app is installed
+
+        if (isQSTileModuleInstalled()) {
+            // Launch configuration activity
+            Intent intent = new Intent();
+            intent.setComponent(new ComponentName("com.itachi1706.cheesecakeutilities_tiles", "com.itachi1706.cheesecakeutilities_tiles.ConfigurationActivity"));
+            startActivity(intent);
+        } else
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=com.itachi1706.cheesecakeutilities_tiles"))); // Launch Play Store
+
+        finish();
+    }
+
+    private boolean isQSTileModuleInstalled() {
+        PackageManager packageManager = getPackageManager();
+        try {
+            packageManager.getPackageInfo("com.itachi1706.cheesecakeutilities_tiles", 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+}
