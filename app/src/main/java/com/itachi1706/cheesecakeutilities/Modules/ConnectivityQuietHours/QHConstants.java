dc3f3d65907f58190ef784e4d47ecb9bc9374b26
itachi1706 <kennethsohyq@gmail.com>
2017-04-28 22:30:35 +0800

    Started work on Bluetooth Quiet Hours logic
    Also added wifi quiet hours alarm scheduling, though the receiver is just a stub for now
    And fixed some bugs. lol

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/ConnectivityQuietHours/QHConstants.java
@@ -0,0 +1,15 @@
+package com.itachi1706.cheesecakeutilities.Modules.ConnectivityQuietHours;
+
+/**
+ * Created by Kenneth on 28/4/2017.
+ * for com.itachi1706.cheesecakeutilities.Modules.ConnectivityQuietHours in CheesecakeUtilities
+ */
+
+public class QHConstants {
+
+    public static final String QH_BT_STATE = "quiethour_bt_status", QH_WIFI_STATE = "quiethour_wifi_status";
+    public static final String QH_BT_TIME = "quiethour_bt", QH_WIFI_TIME = "quiethour_wifi";
+    public static final String QH_BT_NOTIFICATION = "quiethour_bt_notification", QH_WIFI_NOTIFICATION = "quiethour_wifi_notification";
+
+    public static final int QH_NOTIFY_NEVER = 0, QH_NOTIFY_WHEN_TRIGGERED = 1, QH_NOTIFY_ALWAYS = 2, QH_NOTIFY_DEBUG = 3;
+}
