743c9cd07155b3119a1d6932c200a6c31a4d23aa
itachi1706 <kennethsohyq@gmail.com>
2017-05-21 20:21:57 +0800

    Updated Google Services and removed unnecessary imports

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/HtcSerialIdentification/HtcSerialIdentificationActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/HtcSerialIdentification/HtcSerialIdentificationActivity.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
