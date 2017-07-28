732ebbe3f47615f2c76a44ec1679b9f769c5d21b
itachi1706 <kennethsohyq@gmail.com>
2017-07-28 19:17:28 +0800

    Got the date time pickers working for mileage
    Now from time and to time will bring both date and time pickers up and return result in millis to process and send to Firebase

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/VehicleMileageTracker/AddNewMileageRecordActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/VehicleMileageTracker/AddNewMileageRecordActivity.java
@@ -19,10 +19,15 @@ import com.google.firebase.database.ValueEventListener;
 import com.itachi1706.cheesecakeutilities.Modules.VehicleMileageTracker.Objects.Vehicle;
 import com.itachi1706.cheesecakeutilities.Modules.VehicleMileageTracker.Objects.VehicleClass;
 import com.itachi1706.cheesecakeutilities.R;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
+import java.util.Locale;
 
 public class AddNewMileageRecordActivity extends AppCompatActivity {
 
@@ -73,6 +78,118 @@ public class AddNewMileageRecordActivity extends AppCompatActivity {
 
             }
         });
+
+        // Date Time Pickers
+        timeFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFromDate();
+            }
+        });
+        timeTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setToDate();
+            }
+        });
+    }
+
+    private long fromTimeVal = 0, toTimeVal = 0;
+
+    private void setFromDate() {
+        Calendar cal = Calendar.getInstance();
+        DatePickerDialog dpd = DatePickerDialog.newInstance(new FromTimeDate(),
+                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        dpd.show(getFragmentManager(), "DatepickerdialogF");
+        dpd.dismissOnPause(true);
+    }
+
+    private void setFromTime() {
+        Calendar cal = Calendar.getInstance();
+        TimePickerDialog tpd = TimePickerDialog.newInstance(new FromTimeDate(),
+                cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true);
+        tpd.show(getFragmentManager(), "TimepickerdialogF");
+        tpd.dismissOnPause(true);
+    }
+
+    private void processFromTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy HH:mm", Locale.US);
+        Date dt = new Date();
+        dt.setTime(fromTimeVal);
+        String date = sdf.format(dt);
+        timeFrom.setText(date + " hrs");
+    }
+
+    private class FromTimeDate implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(fromTimeVal);
+            c.set(Calendar.YEAR, year);
+            c.set(Calendar.MONTH, monthOfYear);
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            fromTimeVal = c.getTimeInMillis();
+            setFromTime();
+        }
+
+        @Override
+        public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(fromTimeVal);
+            c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            c.set(Calendar.MINUTE, minute);
+            fromTimeVal = c.getTimeInMillis();
+            processFromTime();
+        }
+    }
+
+    private void setToDate() {
+        Calendar cal = Calendar.getInstance();
+        DatePickerDialog dpd = DatePickerDialog.newInstance(new ToTimeDate(),
+                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+        dpd.show(getFragmentManager(), "DatepickerdialogT");
+        dpd.dismissOnPause(true);
+    }
+
+    private void setToTime() {
+        Calendar cal = Calendar.getInstance();
+        TimePickerDialog tpd = TimePickerDialog.newInstance(new ToTimeDate(),
+                cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true);
+        tpd.show(getFragmentManager(), "TimepickerdialogT");
+        tpd.dismissOnPause(true);
+    }
+
+    private void processToTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM yyyy HH:mm", Locale.US);
+        Date dt = new Date();
+        dt.setTime(toTimeVal);
+        String date = sdf.format(dt);
+        timeTo.setText(date + " hrs");
+    }
+
+    private class ToTimeDate implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(toTimeVal);
+            c.set(Calendar.YEAR, year);
+            c.set(Calendar.MONTH, monthOfYear);
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            toTimeVal = c.getTimeInMillis();
+            setToTime();
+        }
+
+        @Override
+        public void onTimeSet(TimePickerDialog view, int hourOfDay, int minute, int second) {
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(toTimeVal);
+            c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            c.set(Calendar.MINUTE, minute);
+            toTimeVal = c.getTimeInMillis();
+            processToTime();
+        }
     }
 
     private void refreshVehicles(final DatabaseReference vehicles) {
