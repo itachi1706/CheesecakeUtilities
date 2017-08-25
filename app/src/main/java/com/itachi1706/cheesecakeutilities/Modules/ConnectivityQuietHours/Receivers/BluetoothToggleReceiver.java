1f47d57c0a8180d35970fdb62fc0490e62d9a8fe
itachi1706 <kennethsohyq@gmail.com>
2017-08-25 23:41:11 +0800

    Compiling against Android Oreo (API 26) SDK - Making notifications work again - Updating updater to reuse new updater

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/ConnectivityQuietHours/Receivers/BluetoothToggleReceiver.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/ConnectivityQuietHours/Receivers/BluetoothToggleReceiver.java
@@ -1,5 +1,6 @@
 package com.itachi1706.cheesecakeutilities.Modules.ConnectivityQuietHours.Receivers;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
@@ -8,10 +9,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.itachi1706.appupdater.Util.UpdaterHelper;
 import com.itachi1706.cheesecakeutilities.Modules.ConnectivityQuietHours.ConnectivityQuietHoursActivity;
 import com.itachi1706.cheesecakeutilities.Modules.ConnectivityQuietHours.QHConstants;
 import com.itachi1706.cheesecakeutilities.R;
@@ -61,7 +64,14 @@ public class BluetoothToggleReceiver extends BroadcastReceiver {
     private void sendNotification(Context context, int notificationLevel, boolean workDone, boolean btState) {
         String time = DateFormat.getTimeInstance().format(new Date(System.currentTimeMillis()));
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+        // Create the Notification Channel
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(QHConstants.QH_NOTIFICATION_CHANNEL, QHConstants.QH_NOTIFICATION_CHANNEL_TITLE, NotificationManager.IMPORTANCE_DEFAULT);
+            mChannel.setDescription(QHConstants.QH_NOTIFICATION_CHANNEL_DESC);
+            mChannel.enableLights(false);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, QHConstants.QH_NOTIFICATION_CHANNEL);
         mBuilder.setSmallIcon(R.drawable.notification_icon).setContentTitle("Bluetooth Quiet Hour " + ((btState) ? "Enabled" : "Disabled"))
                 .setContentText("Bluetooth state toggled on " + time)
                 .setAutoCancel(true)
