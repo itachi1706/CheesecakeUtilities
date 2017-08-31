b36f4e9f545beac542ad6493595942650edb8ec0
itachi1706 <kennethsohyq@gmail.com>
2017-08-31 23:03:56 +0800

    Added basic statistics that should be very modular due to the way it is designed to be. Hopefully

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/VehicleMileageTracker/Fragments/VehicleMileageGeneralStatsFragment.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/VehicleMileageTracker/Fragments/VehicleMileageGeneralStatsFragment.java
@@ -1,21 +1,30 @@
 package com.itachi1706.cheesecakeutilities.Modules.VehicleMileageTracker.Fragments;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.util.ArrayMap;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.itachi1706.cheesecakeutilities.Objects.DualLineString;
 import com.itachi1706.cheesecakeutilities.R;
 import com.itachi1706.cheesecakeutilities.RecyclerAdapters.DualLineStringRecyclerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Kenneth on 31/8/2017.
@@ -25,6 +34,10 @@ import java.util.List;
 public class VehicleMileageGeneralStatsFragment extends Fragment {
 
     DualLineStringRecyclerAdapter adapter;
+    SharedPreferences sp;
+
+    private boolean ready = false;
+    private ArrayMap<String, String> legend;
 
 
     public VehicleMileageGeneralStatsFragment() {
@@ -48,8 +61,27 @@ public class VehicleMileageGeneralStatsFragment extends Fragment {
 
             // Set up blank layout for now
             List<DualLineString> placeholder = new ArrayList<>();
+            placeholder.add(new DualLineString("Loading...", "Calculating statistics..."));
             adapter = new DualLineStringRecyclerAdapter(placeholder, false);
             recyclerView.setAdapter(adapter);
+
+            sp = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+            FirebaseDatabase.getInstance().getReference().child("stat-legend").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    legend = new ArrayMap<>();
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        legend.put(ds.getKey(), ds.getValue(String.class));
+                    }
+                    ready = true;
+                    updateStats();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    ready = false;
+                }
+            });
         }
 
         return v;
@@ -58,11 +90,33 @@ public class VehicleMileageGeneralStatsFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        updateStats();
+        if (ready) updateStats();
     }
 
     public void updateStats() {
+        final String user_id = sp.getString("firebase_uid", "nien");
+        if (user_id.equalsIgnoreCase("nien")) {
+            // Fail, return to login activity
+            Toast.makeText(getActivity(), "Invalid Login Token, please re-login", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        FirebaseDatabase.getInstance().getReference().child("users").child(user_id).child("records").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<DualLineString> stats = new ArrayList<>();
+                for (Map.Entry<String, String> i : legend.entrySet()) {
+                    if (!dataSnapshot.hasChild(i.getKey())) continue;
+                    stats.add(new DualLineString(i.getValue(), dataSnapshot.child(i.getKey()).getValue(Double.class) + " km"));
+                }
+                adapter.update(stats);
+                adapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
