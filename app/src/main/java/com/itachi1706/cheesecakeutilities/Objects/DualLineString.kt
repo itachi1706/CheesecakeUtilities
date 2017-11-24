5fe68976bebb00b1c2c96d0da8f1981e399c9185
itachi1706 <kennethsohyq@gmail.com>
2017-11-24 21:54:36 +0800

    Converted main objects to Kotlin

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Objects/DualLineString.kt
@@ -0,0 +1,9 @@
+package com.itachi1706.cheesecakeutilities.Objects
+
+/**
+ * Created by Kenneth on 29/4/2017.
+ * Modified to Kotlin on 24/11/2017.
+ * for com.itachi1706.cheesecakeutilities.Objects in CheesecakeUtilities
+ */
+
+class DualLineString(var main: String?, var sub: String?)
