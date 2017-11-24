b9a03bec9bee503a77c7dc0726e1ca37dcbe5f01
itachi1706 <kennethsohyq@gmail.com>
2017-11-24 23:05:46 +0800

    Convert JSON objects to use Kotlin instead

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/IPPTCalculator/JsonObjects/Gender.kt
@@ -0,0 +1,13 @@
+package com.itachi1706.cheesecakeutilities.Modules.IPPTCalculator.JsonObjects
+
+import com.google.gson.JsonObject
+
+/**
+ * Created by Kenneth on 7/9/2016.
+ * for com.itachi1706.cheesecakeutilities.Modules.IPPTCalculator.JsonObjects in CheesecakeUtilities
+ */
+class Gender {
+    val run: JsonObject? = null
+    val situps: JsonObject? = null
+    val pushups: JsonObject? = null
+}
