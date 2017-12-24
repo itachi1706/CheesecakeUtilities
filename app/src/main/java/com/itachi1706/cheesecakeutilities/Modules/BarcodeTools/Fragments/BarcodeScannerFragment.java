6d725f97ae236969372df92f5519e41e83083125
itachi1706 <kennethsohyq@gmail.com>
2017-12-24 19:00:49 +0800

    Converted to a fragment layout to supplement future generation of barcodes

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/BarcodeTools/Fragments/BarcodeScannerFragment.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/BarcodeTools/Fragments/BarcodeScannerFragment.java
@@ -1,20 +1,33 @@
-package com.itachi1706.cheesecakeutilities.Modules.BarcodeTools;
+package com.itachi1706.cheesecakeutilities.Modules.BarcodeTools.Fragments;
 
 import android.app.admin.DevicePolicyManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
-import com.itachi1706.cheesecakeutilities.BaseActivity;
+import com.itachi1706.cheesecakeutilities.Modules.BarcodeTools.BarcodeCaptureActivity;
+import com.itachi1706.cheesecakeutilities.Modules.BarcodeTools.BarcodeHelper;
 import com.itachi1706.cheesecakeutilities.R;
 
-public class BarcodeScannerActivity extends BaseActivity {
+import static android.content.Context.DEVICE_POLICY_SERVICE;
+
+/**
+ * Created by Kenneth on 24/12/2017.
+ * for com.itachi1706.cheesecakeutilities.Modules.BarcodeTools.Fragments in CheesecakeUtilities
+ */
+
+public class BarcodeScannerFragment extends Fragment {
 
     // use a compound button so either checkbox or switch widgets work.
     private CompoundButton useFlash;
@@ -26,35 +39,30 @@ public class BarcodeScannerActivity extends BaseActivity {
     private static final String TAG = "BarcodeScanner";
 
     @Override
-    public String getHelpDescription() {
-        return "Utility used to create or scan barcode (2D/3D)\nExamples: ISBN, QR Code etc";
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_barcode_scanner, container, false);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_barcode_scanner);
-
-        statusMessage = findViewById(R.id.status_message);
-        barcodeValue = findViewById(R.id.barcode_value);
-
-        useFlash = findViewById(R.id.use_flash);
-
-        scan = findViewById(R.id.read_barcode);
-        scan.setOnClickListener(v -> {
+        statusMessage = v.findViewById(R.id.status_message);
+        barcodeValue = v.findViewById(R.id.barcode_value);
+        useFlash = v.findViewById(R.id.use_flash);
+        scan = v.findViewById(R.id.read_barcode);
+        scan.setOnClickListener(view -> {
             // launch barcode activity.
-            Intent intent = new Intent(getApplicationContext(), BarcodeCaptureActivity.class);
+            Intent intent = new Intent(getActivity(), BarcodeCaptureActivity.class);
             intent.putExtra(BarcodeCaptureActivity.UseFlash, useFlash.isChecked());
             startActivityForResult(intent, RC_BARCODE_CAPTURE);
         });
+        return v;
     }
 
     @Override
-    protected void onResume() {
+    public void onResume() {
         super.onResume();
         // Check for camera source
-        DevicePolicyManager devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
-        if (!this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) ||
+        DevicePolicyManager devicePolicyManager = (DevicePolicyManager) getActivity().getSystemService(DEVICE_POLICY_SERVICE);
+        if (!this.getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) ||
                 (devicePolicyManager != null && devicePolicyManager.getCameraDisabled(null))) {
             // Disables stuff
             scan.setEnabled(false);
@@ -68,14 +76,14 @@ public class BarcodeScannerActivity extends BaseActivity {
 
     // Returning barcode activity
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RC_BARCODE_CAPTURE) {
             if (resultCode == CommonStatusCodes.SUCCESS) {
                 if (data != null) {
                     Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
                     statusMessage.setText(R.string.barcode_success);
                     StringBuilder result = new StringBuilder();
-                    result.append("Format: ").append(getFormatName(barcode.format)).append("\n\n");
+                    result.append("Format: ").append(BarcodeHelper.getFormatName(barcode.format)).append("\n\n");
                     result.append("Content: ").append(barcode.displayValue).append("\n\n");
                     result.append("Raw Value: ").append(barcode.rawValue).append("\n");
                     barcodeValue.setText(result);
@@ -90,39 +98,6 @@ public class BarcodeScannerActivity extends BaseActivity {
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    private String getFormatName(int format) {
-        switch (format) {
-            case Barcode.CODE_128:
-                return "CODE_128";
-            case Barcode.CODE_39:
-                return "CODE_39";
-            case Barcode.CODE_93:
-                return "CODE_93";
-            case Barcode.CODABAR:
-                return "CODABAR";
-            case Barcode.DATA_MATRIX:
-                return "DATA_MATRIX";
-            case Barcode.EAN_13:
-                return "EAN_13";
-            case Barcode.EAN_8:
-                return "EAN_8";
-            case Barcode.ITF:
-                return "ITF";
-            case Barcode.QR_CODE:
-                return "QR_CODE";
-            case Barcode.UPC_A:
-                return "UPC_A";
-            case Barcode.UPC_E:
-                return "UPC_E";
-            case Barcode.PDF417:
-                return "PDF417";
-            case Barcode.AZTEC:
-                return "AZTEC";
-            default:
-                return "Unknown (" + format + ")";
         }
     }
 }
