508b97b9ea61a3f1ad8ad9dfa3c986c95188b491
itachi1706 <kennethsohyq@gmail.com>
2017-12-24 21:35:22 +0800

    Completed validation checks

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/BarcodeTools/BarcodeHelper.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/BarcodeTools/BarcodeHelper.java
@@ -41,23 +41,19 @@ public class BarcodeHelper {
         }
     }
 
-    public static final int GEN_QR = 0;
-    public static final int GEN_UPC_A = 1;
-    public static final int GEN_EAN_13 = 2;
-    public static final int GEN_CODE_39 = 3;
-    public static final int GEN_CODE_93 = 4;
-    public static final int GEN_CODE_128 = 5;
-    public static final int GEN_CODABAR = 6;
-    public static final int GEN_ITF = 7;
-    public static final int GEN_DATA_MATRIX = 8;
-    public static final int GEN_PDF_417 = 9;
-    public static final int GEN_UPC_E = 10;
-    public static final int GEN_EAN_8 = 11;
-    public static final int GEN_AZTEC = 12;
-    public static final int GEN_MAXICODE = 13;
-    public static final int GEN_RSS_14 = 14;
-    public static final int GEN_RSS_EXPANDED = 15;
-    public static final int GEN_UPC_EAN_EXTENSION = 16;
+    private static final int GEN_QR = 0;
+    private static final int GEN_UPC_A = 1;
+    private static final int GEN_EAN_13 = 2;
+    private static final int GEN_CODE_39 = 3;
+    private static final int GEN_CODE_93 = 4;
+    private static final int GEN_CODE_128 = 5;
+    private static final int GEN_CODABAR = 6;
+    private static final int GEN_ITF = 7;
+    private static final int GEN_DATA_MATRIX = 8;
+    private static final int GEN_PDF_417 = 9;
+    private static final int GEN_UPC_E = 10;
+    private static final int GEN_EAN_8 = 11;
+    private static final int GEN_AZTEC = 12;
 
     public static BarcodeFormat getGenerateType(int generate) {
         switch (generate) {
@@ -68,17 +64,63 @@ public class BarcodeHelper {
             case GEN_CODE_128: return BarcodeFormat.CODE_128;
             case GEN_CODABAR: return BarcodeFormat.CODABAR;
             case GEN_ITF: return BarcodeFormat.ITF;
-            case GEN_DATA_MATRIX: return BarcodeFormat.DATA_MATRIX;
+            case GEN_DATA_MATRIX: return BarcodeFormat.DATA_MATRIX; // TODO: Need to be larger
             case GEN_PDF_417: return BarcodeFormat.PDF_417;
             case GEN_UPC_E: return BarcodeFormat.UPC_E;
             case GEN_EAN_8: return BarcodeFormat.EAN_8;
             case GEN_AZTEC: return BarcodeFormat.AZTEC;
-            case GEN_MAXICODE: return BarcodeFormat.MAXICODE;
-            case GEN_RSS_14: return BarcodeFormat.RSS_14;
-            case GEN_RSS_EXPANDED: return BarcodeFormat.RSS_EXPANDED;
-            case GEN_UPC_EAN_EXTENSION: return BarcodeFormat.UPC_EAN_EXTENSION;
             case GEN_QR:
             default: return BarcodeFormat.QR_CODE;
+        }
+    }
+
+    private static final String REGEX_NUM = "^[0-9]*$";
+    private static final String REGEX_NUM_CAPS_ALPHA = "^[0-9A-Z-.$/+% ]*$";
+    private static final String REGEX_NUM_SYMBOL = "^[0-9$-:/.+]*$";
+    private static final String REGEX_ABCD = "^[ABCD]*$";
+    /**
+     * Check if value is validated
+     * @param generatedCode Barcode Type
+     * @param value String to validate
+     * @return null if validated, else String of error message
+     */
+    public static String checkValidation(int generatedCode, String value) {
+        switch (generatedCode) {
+            case GEN_UPC_A:
+                if (value.length() != 11) return "Must be 11 characters long";
+                if (value.matches(REGEX_NUM)) return null;
+                return "Must only contain numbers";
+            case GEN_EAN_13:
+                if (value.length() != 12) return "Must be 12 characters long";
+                if (value.matches(REGEX_NUM)) return null;
+                return "Must only contain numbers";
+            case GEN_CODE_39:
+            case GEN_CODE_93:
+                if (value.matches(REGEX_NUM_CAPS_ALPHA)) return null;
+                return "Can only contain 0–9, A-Z, -.$/+% and space ONLY";
+            case GEN_CODABAR:
+                if (value.matches(REGEX_NUM_SYMBOL)) return null;
+                if (Character.toString(value.charAt(0)).matches(REGEX_ABCD) && Character.toString(value.charAt(value.length() - 1)).matches(REGEX_ABCD)) return null;
+                return "0–9, –$:/.+ only, ABCD can be used at start and end of input";
+            case GEN_ITF:
+                if (!value.matches(REGEX_NUM)) return "Must only contain numbers";
+                if (value.length() % 2 != 0) return "Input must be of even length";
+                return null;
+            case GEN_UPC_E:
+                if (value.length() != 5) return "Must be 5 characters long";
+                if (value.matches(REGEX_NUM)) return null;
+                else return "Must only contain numbers";
+            case GEN_EAN_8:
+                if (value.length() != 7) return "Must be 7 characters long";
+                if (value.matches(REGEX_NUM)) return null;
+                else return "Must only contain numbers";
+            // No need for validation
+            case GEN_AZTEC:
+            case GEN_DATA_MATRIX:
+            case GEN_PDF_417:
+            case GEN_CODE_128:
+            case GEN_QR:
+            default: return null;
         }
     }
 }
