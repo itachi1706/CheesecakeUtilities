faa64a1e7d90803d854ab2b56a1b886a786c7a15
itachi1706 <kennethsohyq@gmail.com>
2017-12-26 16:44:15 +0800

    Added lyrics into the view that will be automatically retrieved when a compatible media is played
    Also added groundworks for a future notification to retrieve lyrics by ignoring execution of all notifications originating from the app (to prevent a loop from occuring caused by the code executing due to code it executed)
    
    More or less done #89. All thats left is fine tuning, add some cool stuff and fix its name and stuff

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Objects/ApiResult.kt
@@ -0,0 +1,13 @@
+package com.itachi1706.cheesecakeutilities.Objects
+
+/**
+ * Created by Kenneth on 26/12/2017.
+ * for com.itachi1706.cheesecakeutilities.Objects in CheesecakeUtilities
+ */
+
+class ApiResult {
+
+    val msg: String? = null
+    val error: Int = 0
+
+}
--- app/src/main/java/com/itachi1706/cheesecakeutilities/Util/CommonVariables.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Util/CommonVariables.java
@@ -8,7 +8,8 @@ import static android.graphics.Color.rgb;
  */
 public class CommonVariables {
 
-    public static final String BASE_SERVER_URL = "http://api.itachi1706.com/api/appupdatechecker.php?action=androidretrievedata&packagename=";
+    public static final String BASE_API_URL = "http://api.itachi1706.com/api/";
+    public static final String BASE_SERVER_URL = BASE_API_URL + "appupdatechecker.php?action=androidretrievedata&packagename=";
     public static final int[] MATERIAL_COLORS = {rgb(244, 67, 54), rgb(233, 30, 99), rgb(156, 39, 176), rgb(103, 58, 183),
             rgb(63, 81, 181), rgb(33, 150, 243), rgb(3, 169, 244), rgb(0, 188, 212), rgb(0, 150, 136), rgb(76, 175, 80),
             rgb(139, 195, 74), rgb(205, 220, 57), rgb(255, 235, 59), rgb(255, 193, 7), rgb(255, 152, 0), rgb(255, 87, 34),
