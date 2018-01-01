b7313e7c18aba6bc7b2100dfa67a89608b85025d
itachi1706 <kennethsohyq@gmail.com>
2018-01-01 18:14:19 +0800

    Removed unnecessary import

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/LyricFinder/RetrieveLyricTask.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/LyricFinder/RetrieveLyricTask.java
@@ -13,7 +13,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
