1e8c9ca587a7d67462916cd00c9c93d538398e9d
itachi1706 <kennethsohyq@gmail.com>
2018-02-18 16:55:23 +0800

    Allow graph toolbar to collapse as well as detect if its portrait mode and notify that graphs work better in landscape

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/SGPsi/PsiGraphFragment.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/SGPsi/PsiGraphFragment.java
@@ -2,6 +2,7 @@ package com.itachi1706.cheesecakeutilities.Modules.SGPsi;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,16 +18,14 @@ import com.itachi1706.cheesecakeutilities.R;
 
 public class PsiGraphFragment extends Fragment {
 
-    private WebView webView;
-    private String key;
-
     @SuppressLint("SetJavaScriptEnabled")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_chromium_trex, container, false);
-        webView = view.findViewById(R.id.webview);
+        WebView webView = view.findViewById(R.id.webview);
 
         Bundle b = getArguments();
+        String key;
         if (b != null) key = b.getString("key", "psi");
         else key = "psi";
         String filename = key + "graph.html";
