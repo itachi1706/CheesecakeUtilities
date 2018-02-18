ddef94a9886549b5b3aeae24861c8af524854f17
itachi1706 <kennethsohyq@gmail.com>
2018-02-18 14:10:38 +0800

    Created the PSI object

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/SGPsi/PsiGeneral.kt
@@ -0,0 +1,25 @@
+package com.itachi1706.cheesecakeutilities.Modules.SGPsi
+
+/**
+ * Created by Kenneth on 18/2/2018.
+ * for com.itachi1706.cheesecakeutilities.Modules.SGPsi in CheesecakeUtilities
+ */
+
+class PsiGeneral {
+
+    val psirange: String? = null
+    val particlerange: String? = null
+    val time: String? = null
+    val threehr: Int = 0
+    val north: Int = 0
+    val south: Int = 0
+    val east: Int = 0
+    val west: Int = 0
+    val central: Int = 0
+    val particlenorth: Int = 0
+    val particlesouth: Int = 0
+    val particleeast: Int = 0
+    val particlewest: Int = 0
+    val particlecentral: Int = 0
+    val rawtimestamp: Long = 0
+}
