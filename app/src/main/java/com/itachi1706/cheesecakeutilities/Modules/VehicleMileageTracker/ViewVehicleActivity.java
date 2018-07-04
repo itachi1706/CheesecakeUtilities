9e410508fea2e500106985382407da8f9a58bce0
itachi1706 <kennethsohyq@gmail.com>
2018-07-04 21:52:01 +0800

    Table will now auto refresh after an edit or delete job
    Finished the edit/delete vehicle part of #120

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/VehicleMileageTracker/ViewVehicleActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/VehicleMileageTracker/ViewVehicleActivity.java
@@ -25,6 +25,8 @@ import java.util.List;
 
 public class ViewVehicleActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,53 +36,21 @@ public class ViewVehicleActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView = findViewById(R.id.recycler_view);
         if (recyclerView != null) {
             recyclerView.setHasFixedSize(true);
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
             linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
             recyclerView.setLayoutManager(linearLayoutManager);
             recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-            // Set up layout
-            FirebaseUtils.getFirebaseDatabase().getReference().child("vehicles").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (!dataSnapshot.hasChildren()) return;
-                    List<DualLineString> vehicleList = new ArrayList<>();
-                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                        if (ds.hasChildren()) {
-                            for (DataSnapshot d : ds.getChildren()) {
-                                Vehicle v = d.getValue(Vehicle.class);
-                                if (v == null) continue;
-                                vehicleList.add(new DualLineString(v.getName(), v.getShortname() + " (" + v.getVehicleClass().toUpperCase() + ")"));
-                            }
-                        }
-                    }
-                    DualLineStringRecyclerAdapter adapter = new DualLineStringRecyclerAdapter(vehicleList, false);
-                    adapter.setOnCreateContextMenuListener((menu, v, menuInfo) -> {
-                        TextView test = v.findViewById(android.R.id.text2);
-                        String[] tmp = test.getText().toString().split(" ");
-                        StringBuilder strBuilder = new StringBuilder();
-                        for (int i = 0; i < tmp.length - 1; i++) {
-                            strBuilder.append(tmp[i]).append(" ");
-                        }
-                        selectEdit = strBuilder.toString().trim().replaceAll("[^A-Za-z0-9]", "");
-                        selectClass = tmp[tmp.length - 1].substring(1, tmp[tmp.length - 1].length() - 1).toLowerCase();
-                        menu.setHeaderTitle("Edit Vehicle " + selectEdit);
-                        menu.add("Edit");
-                        menu.add("Delete");
-                    });
-                    recyclerView.setAdapter(adapter);
-                }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                    Log.w("ViewVehicleAct", "loadVehicles:onCancelled", databaseError.toException());
-                }
-            });
-
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Set up layout
+        if (recyclerView != null) updateRecords(recyclerView);
     }
 
     private String selectEdit, selectClass;
@@ -102,6 +72,7 @@ public class ViewVehicleActivity extends AppCompatActivity {
                         FirebaseUtils.getFirebaseDatabase().getReference().child("vehicles")
                                 .child(selectClass).child(selectEdit).removeValue();
                         Toast.makeText(getApplicationContext(), "Vehicle Deleted", Toast.LENGTH_SHORT).show();
+                        updateRecords(recyclerView);
                     }).show();
             return true;
         }
@@ -117,5 +88,44 @@ public class ViewVehicleActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void updateRecords(RecyclerView recyclerView) {
+        FirebaseUtils.getFirebaseDatabase().getReference().child("vehicles").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (!dataSnapshot.hasChildren()) return;
+                List<DualLineString> vehicleList = new ArrayList<>();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    if (ds.hasChildren()) {
+                        for (DataSnapshot d : ds.getChildren()) {
+                            Vehicle v = d.getValue(Vehicle.class);
+                            if (v == null) continue;
+                            vehicleList.add(new DualLineString(v.getName(), v.getShortname() + " (" + v.getVehicleClass().toUpperCase() + ")"));
+                        }
+                    }
+                }
+                DualLineStringRecyclerAdapter adapter = new DualLineStringRecyclerAdapter(vehicleList, false);
+                adapter.setOnCreateContextMenuListener((menu, v, menuInfo) -> {
+                    TextView test = v.findViewById(android.R.id.text2);
+                    String[] tmp = test.getText().toString().split(" ");
+                    StringBuilder strBuilder = new StringBuilder();
+                    for (int i = 0; i < tmp.length - 1; i++) {
+                        strBuilder.append(tmp[i]).append(" ");
+                    }
+                    selectEdit = strBuilder.toString().trim().replaceAll("[^A-Za-z0-9]", "");
+                    selectClass = tmp[tmp.length - 1].substring(1, tmp[tmp.length - 1].length() - 1).toLowerCase();
+                    menu.setHeaderTitle("Edit Vehicle " + selectEdit);
+                    menu.add("Edit");
+                    menu.add("Delete");
+                });
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("ViewVehicleAct", "loadVehicles:onCancelled", databaseError.toException());
+            }
+        });
     }
 }
