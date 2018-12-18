184904995dff963d9b1de54d83f4eb7d1cae7ff2
itachi1706 <kennethsohyq@gmail.com>
2018-12-18 22:20:02 +0800

    Added authentication with device screen lock
    Also IF both options are selected, the fingerprint authentication is preferred. And should you get locked out, instead of showing you the lock out message prompt, the app will fall back to device screen lock
    
    Closes #222

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Features/FingerprintAuth/AuthenticationActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Features/FingerprintAuth/AuthenticationActivity.java
@@ -1,10 +1,11 @@
 package com.itachi1706.cheesecakeutilities.Features.FingerprintAuth;
 
 import android.annotation.SuppressLint;
+import android.app.KeyguardManager;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -16,6 +17,8 @@ import com.itachi1706.cheesecakeutilities.R;
 import java.util.concurrent.Executor;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometrics.BiometricConstants;
@@ -26,6 +29,8 @@ public class AuthenticationActivity extends AppCompatActivity {
     SharedPreferences sp;
     FirebaseAnalytics mFirebaseAnalytics;
     Context mContext;
+
+    public static final int INTENT_AUTH_SL = 2;
 
     private static final String TAG = "Authentication", INTENT_MSG = "message";
 
@@ -44,12 +49,30 @@ public class AuthenticationActivity extends AppCompatActivity {
             BiometricPrompt p = new BiometricPrompt(this, executor, callback);
             BiometricPrompt.PromptInfo promptInfo = BiometricCompatHelper.createPromptObject();
             p.authenticate(promptInfo);
+        } else if (BiometricCompatHelper.isScreenLockProtectionEnabled(this) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            authWithScreenLock();
         } else {
             // No biometric data, treat as authenticated
             Log.i(TAG, "No Biometric Authentication Found. Presuming Authenticated");
             setResult(RESULT_OK);
             finish();
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private void authWithScreenLock() {
+        KeyguardManager km = (KeyguardManager) getSystemService(KEYGUARD_SERVICE);
+        if (km == null) {
+            Toast.makeText(mContext, R.string.dialog_cancelled, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent();
+            Log.e(TAG, "Authentication Error (KEYGUARD NULL): KeyGuard is not ready");
+            intent.putExtra(INTENT_MSG, "Authentication Error: KeyGuard is not ready");
+            setResult(RESULT_CANCELED, intent);
+            finish();
+            return;
+        }
+        Intent slIntent = km.createConfirmDeviceCredentialIntent("Sign In", "Unlock your screen again to continue");
+        startActivityForResult(slIntent, INTENT_AUTH_SL);
     }
 
     private void migrateToBiometric() {
@@ -81,19 +104,20 @@ public class AuthenticationActivity extends AppCompatActivity {
                         return;
                     case BiometricConstants.ERROR_LOCKOUT:
                     case BiometricConstants.ERROR_LOCKOUT_PERMANENT:
-                        Toast.makeText(mContext, R.string.dialog_cancelled, Toast.LENGTH_SHORT).show();
-                        Log.i(TAG, "User Lock out");
-                        intent.putExtra(INTENT_MSG, "Lockout");
-                        new AlertDialog.Builder(mContext).setTitle("Fingerprint sensor disabled (Locked out)")
-                                .setMessage("You have scanned an invalid fingerprint too many times and your fingerprint sensor has been disabled. \n\n" +
-                                        "Please re-authenticate by unlocking or rebooting your phone again or disable fingerprints on your device")
-                                .setCancelable(false).setPositiveButton(R.string.dialog_action_positive_close, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
+                        if (BiometricCompatHelper.isScreenLockProtectionEnabled(mContext) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                            authWithScreenLock();
+                        } else {
+                            Toast.makeText(mContext, R.string.dialog_cancelled, Toast.LENGTH_SHORT).show();
+                            Log.i(TAG, "User Lock out");
+                            intent.putExtra(INTENT_MSG, "Lockout");
+                            new AlertDialog.Builder(mContext).setTitle("Fingerprint sensor disabled (Locked out)")
+                                    .setMessage("You have scanned an invalid fingerprint too many times and your fingerprint sensor has been disabled. \n\n" +
+                                            "Please re-authenticate by unlocking or rebooting your phone again or disable fingerprints on your device")
+                                    .setCancelable(false).setPositiveButton(R.string.dialog_action_positive_close, (dialog, which) -> {
                                 setResult(RESULT_CANCELED, intent);
                                 finish();
-                            }
-                        }).show();
+                            }).show();
+                        }
                         return;
                     default:
                         Toast.makeText(mContext, R.string.dialog_cancelled, Toast.LENGTH_SHORT).show();
@@ -123,4 +147,26 @@ public class AuthenticationActivity extends AppCompatActivity {
             Log.i(TAG, "Wrong Biometric detected");
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == INTENT_AUTH_SL) {
+            switch (resultCode) {
+                case RESULT_OK:
+                    Toast.makeText(mContext, R.string.dialog_authenticated, Toast.LENGTH_LONG).show();
+                    Log.i(TAG, "User Authenticated");
+                    setResult(RESULT_OK);
+                    mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.LOGIN, null);
+                    finish();
+                    break;
+                default:
+                    Intent intent = new Intent();
+                    Toast.makeText(mContext, R.string.dialog_cancelled, Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Authentication Error (" + resultCode + "): ACTION_FAILED_OR_CANCELLED");
+                    intent.putExtra(INTENT_MSG, "Authentication Error: ACTION_FAILED_OR_CANCELLED");
+                    setResult(RESULT_CANCELED, intent);
+                    finish();
+            }
+        } else super.onActivityResult(requestCode, resultCode, data);
+    }
 }
