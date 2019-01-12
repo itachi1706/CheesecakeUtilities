b5167b0664b43a3caf3d1ab5335bb069672eaa40
itachi1706 <kennethsohyq@gmail.com>
2019-01-12 23:30:53 +0800

    Implemented some stuff about the access token

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/MSLIntegration/MSLActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/MSLIntegration/MSLActivity.java
@@ -1,5 +1,6 @@
 package com.itachi1706.cheesecakeutilities.Modules.MSLIntegration;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.itachi1706.appupdater.Util.PrefHelper;
 import com.itachi1706.cheesecakeutilities.BaseActivity;
 import com.itachi1706.cheesecakeutilities.R;
 import com.itachi1706.cheesecakeutilities.RecyclerAdapters.StringRecyclerAdapter;
@@ -26,6 +28,9 @@ public class MSLActivity extends BaseActivity {
     TextInputEditText accessToken;
     Button saveToken, forceSync;
     TextInputLayout til_accessToken;
+    SharedPreferences sp;
+
+    public static final String MSL_SP_ACCESS_TOKEN = "msl_access_token";
 
     @Override
     public String getHelpDescription() {
@@ -47,6 +52,7 @@ public class MSLActivity extends BaseActivity {
         accessToken = findViewById(R.id.et_msl_AT);
         googleSignIn = findViewById(R.id.btn_msl_google);
         til_accessToken = findViewById(R.id.til_et_msl_AT);
+        sp = PrefHelper.getDefaultSharedPreferences(this);
 
         // Setup Layout
         googleSignIn.setColorScheme(SignInButton.COLOR_LIGHT);
@@ -55,7 +61,6 @@ public class MSLActivity extends BaseActivity {
         linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
         history.setLayoutManager(linearLayoutManager);
         history.setItemAnimator(new DefaultItemAnimator());
-        syncCal.setEnabled(false); // TODO: Remove when implemented
 
         String[] loading = new String[1];
         loading[0] = "Loading...";
@@ -74,7 +79,7 @@ public class MSLActivity extends BaseActivity {
         super.onResume();
 
         googleSignIn.setEnabled(true); // TODO: Disable button if valid
-        // TODO: Menu item for Signing out
+        updateToggles();
     }
 
     @Override
@@ -103,12 +108,41 @@ public class MSLActivity extends BaseActivity {
         }
     }
 
-    private void btnSave() {
+    private boolean hasToken() {
+        return sp.contains(MSL_SP_ACCESS_TOKEN);
+    }
 
+    private void updateToggles() {
+        // Only update if OAuth AND access token is found
+        syncTask.setEnabled(false);
+        syncCal.setEnabled(false);
+        if (hasToken()) {
+            // TODO: Add Sync Calendar when implemented
+            // TODO: Add check for OAuth token as well
+            syncTask.setEnabled(true);
+        }
+    }
+
+    private void btnSave() {
+        til_accessToken.setErrorEnabled(false);
+        if (accessToken.getText().toString().isEmpty()) {
+            til_accessToken.setError("Please enter an access token");
+            til_accessToken.setErrorEnabled(true);
+            if (hasToken()) sp.edit().remove(MSL_SP_ACCESS_TOKEN).apply();
+            updateToggles();
+            return;
+        }
+
+        sp.edit().putString(MSL_SP_ACCESS_TOKEN, accessToken.getText().toString().trim()).apply();
+        updateToggles();
+        Toast.makeText(this, "Saved token", Toast.LENGTH_LONG).show();
     }
 
     private void btnSync() {
-
+        if (!hasToken()) {
+            Toast.makeText(this, "No access token", Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 
     private void btnLogin() {
@@ -116,10 +150,17 @@ public class MSLActivity extends BaseActivity {
     }
 
     private void toggleTask(boolean isChecked) {
-
+        if (!hasToken()) {
+            Toast.makeText(this, "Please enter an Access Token to sync your tasks to Google Calendar", Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 
     private void toggleCal(boolean isChecked) {
+        if (!hasToken()) {
+            Toast.makeText(this, "Please enter an Access Token to sync your schedule to Google Calendar", Toast.LENGTH_LONG).show();
+            return;
+        }
         Toast.makeText(this, "Will be implemented in a future release", Toast.LENGTH_LONG).show();
         // TODO: Note
     }
