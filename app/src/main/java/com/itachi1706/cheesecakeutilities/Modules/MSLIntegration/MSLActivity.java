504e685c3e8c2364a0bfeb4fbda3e7decf033113
itachi1706 <kennethsohyq@gmail.com>
2019-01-12 22:47:20 +0800

    Added a new activity for MSL Sync and a rough layout that I will be using for it
    Also added it to the main menu for quick access to it

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/MSLIntegration/MSLActivity.java
@@ -0,0 +1,16 @@
+package com.itachi1706.cheesecakeutilities.Modules.MSLIntegration;
+
+import android.os.Bundle;
+
+import com.itachi1706.cheesecakeutilities.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MSLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msl);
+    }
+}
