181140a53ebf13865545450e63ccb95821891d45
itachi1706 <kennethsohyq@gmail.com>
2019-05-13 23:03:13 +0800

    [TRAVIS] argh forgot a space now ._.

--- travisscript/startemu.sh
+++ travisscript/startemu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $ESPRESSO -eq 1]
+if [ $ESPRESSO -eq 1 ]
 then
     echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI --tag $ANDROID_TAG
     emulator -avd test -no-window &
