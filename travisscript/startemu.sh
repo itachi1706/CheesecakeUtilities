23e94230f2d589b950c56816f9d9fe8f4ac48657
itachi1706 <kennethsohyq@gmail.com>
2019-05-13 22:36:10 +0800

    [TRAVIS] Trying a different way to start emulator
    What we want is really to not start an emulator when it is not needed

---
+++ travisscript/startemu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ $ESPRESSO -eq 1]
+    echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI --tag $ANDROID_TAG
+    emulator -avd test -no-window &
+    android-wait-for-emulator
+fi
