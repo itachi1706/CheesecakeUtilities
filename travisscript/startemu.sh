782badeff6c8acd38753c59b3dce5ccf7d641286
itachi1706 <kennethsohyq@gmail.com>
2019-05-13 22:55:34 +0800

    [TRAVIS] Forgot to "then"

--- travisscript/startemu.sh
+++ travisscript/startemu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ $ESPRESSO -eq 1]
+then
     echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI --tag $ANDROID_TAG
     emulator -avd test -no-window &
     android-wait-for-emulator
