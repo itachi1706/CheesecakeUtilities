1c796d174f01934689bcf62a748d87b42e51c9cc
itachi1706 <kennethsohyq@gmail.com>
2019-06-12 22:19:28 +0800

    Testing initial migration over to the new cepas scanning library

--- app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/CEPASReader/CEPASActivity.java
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/Modules/CEPASReader/CEPASActivity.java
@@ -1,11 +1,12 @@
 package com.itachi1706.cheesecakeutilities.Modules.CEPASReader;
 
+import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
 
-import com.itachi1706.cepaslib.fragment.CEPASCardScanFragment;
+import com.codebutler.farebot.app.feature.main.MainActivity;
 import com.itachi1706.cheesecakeutilities.BaseModuleActivity;
 
 public class CEPASActivity extends BaseModuleActivity {
@@ -18,14 +19,17 @@ public class CEPASActivity extends BaseModuleActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getSupportFragmentManager().findFragmentById(android.R.id.content)==null) {
+        // TODO: Reimplement
+        /*if (getSupportFragmentManager().findFragmentById(android.R.id.content)==null) {
             getSupportFragmentManager().beginTransaction()
                     .add(android.R.id.content, new CEPASCardScanFragment())
                     .commit();
-        }
+        }*/
 
         if (NfcAdapter.getDefaultAdapter(this) == null)
             new AlertDialog.Builder(this).setTitle("NFC Unavailable").setMessage("NFC is not available on this device. This utility will now exit")
                     .setPositiveButton(android.R.string.ok, (dialog, which) -> finish()).setCancelable(false).show();
+        else
+            startActivity(new Intent(this, MainActivity.class));
     }
 }
