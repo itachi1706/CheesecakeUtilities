af3b04fbfdbe05358f7fe118a7477d1b9958811d
itachi1706 <kennethsohyq@gmail.com>
2019-07-08 17:45:57 +0800

    [Third Party] Implemented Spectrum code directly into the app and tweaked so that we can manage and tweak it to our own liking
    The code is managed under the MIT license that allows the modification that we will be doing to it. Original License can be found here: https://github.com/the-blue-alliance/spectrum/blob/master/LICENSE.md

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/extlibs/com/thebluealliance/spectrum/internal/SelectedColorChangedEvent.java
@@ -0,0 +1,21 @@
+package com.itachi1706.cheesecakeutilities.extlibs.com.thebluealliance.spectrum.internal;
+
+import androidx.annotation.ColorInt;
+
+/**
+ * Represents a newly-selected color; used with {@link org.greenrobot.eventbus.EventBus} for
+ * internal communication.
+ */
+public class SelectedColorChangedEvent {
+
+    private @ColorInt int mSelectedColor;
+
+    public SelectedColorChangedEvent(@ColorInt int color) {
+        mSelectedColor = color;
+    }
+
+    public @ColorInt
+    int getSelectedColor() {
+        return mSelectedColor;
+    }
+}
