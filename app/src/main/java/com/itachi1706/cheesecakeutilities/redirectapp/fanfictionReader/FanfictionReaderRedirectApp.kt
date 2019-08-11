e186d8f252eab079b1a34ccb7bd6ebb6709f9a27
itachi1706 <kennethsohyq@gmail.com>
2019-08-11 20:51:06 +0800

    Replaced the menu item with a new "Redirect App" option
    This will attempt to launch the main activity of the FFReader app. Failing that it will ask the user if they wish to install the application or not

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/redirectapp/fanfictionReader/FanfictionReaderRedirectApp.kt
@@ -0,0 +1,46 @@
+package com.itachi1706.cheesecakeutilities.redirectapp.fanfictionReader
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.itachi1706.cheesecakeutilities.R
+import com.itachi1706.cheesecakeutilities.redirectapp.InstallAppTask
+
+
+/**
+ * Created by Kenneth on 11/8/2019.
+ * for com.itachi1706.cheesecakeutilities in CheesecakeUtilities
+ */
+class FanfictionReaderRedirectApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val data = packageManager.getLaunchIntentForPackage("com.itachi1706.fanfictionnetreader")
+        Log.d(TAG, "Has Data: ${data == null}")
+        if (data == null) {
+            Log.i(TAG, "App Not Installed")
+            notInstalled()
+            return
+        }
+        startActivity(data)
+        finish()
+    }
+
+    private fun notInstalled() {
+        // App not installed
+        AlertDialog.Builder(this).setTitle("Fanfiction Reader App not installed")
+                .setMessage("The Fanfiction Reader app is not installed on this device")
+                .setNegativeButton(android.R.string.cancel) { _,_ -> finish() }
+                .setPositiveButton(R.string.install) { _, _ ->
+                    Toast.makeText(this, "Downloading app...", Toast.LENGTH_LONG).show()
+                    InstallAppTask(this).execute()
+                }.show()
+    }
+
+    companion object {
+        private const val TAG = "FanficReaderRedir"
+    }
+}
