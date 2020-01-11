7834335f463c80e6c159e47586f835ce8e2c0f2f
itachi1706 <kennethsohyq@gmail.com>
2020-01-11 23:35:38 +0800

    CUTILAND-254 Converted the utility to kotlin based code
    Cause its Kotlin heheh

---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/modules/unicodeKeyboard/UnicodeActivity.kt
@@ -0,0 +1,60 @@
+package com.itachi1706.cheesecakeutilities.modules.unicodeKeyboard
+
+import android.os.Build
+import android.os.Bundle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
+import com.itachi1706.cheesecakeutilities.BaseModuleActivity
+import com.itachi1706.cheesecakeutilities.R
+import com.itachi1706.cheesecakeutilities.modules.unicodeKeyboard.recyclerAdapters.UnicodeMenuAdapter
+import kotlinx.android.synthetic.main.fragment_recycler_view.*
+
+class UnicodeActivity : BaseModuleActivity() {
+    override val helpDescription: String
+        get() {
+            var message = "List of Unicode emojis that you can click on to copy to your clipboard for use everywhere else :D"
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) message += "\n\nYou can even drag and drop emojis into textboxes when you are in Android's Multi Window mode by long clicking the emoji!"
+            return message
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unicode)
+        main_menu_recycler_view.setHasFixedSize(true)
+        val gridLayoutManager = GridLayoutManager(this, 2)
+        main_menu_recycler_view.layoutManager = gridLayoutManager
+        main_menu_recycler_view.itemAnimator = DefaultItemAnimator()
+        // Set up layout
+        val menuitems = unicodeItems()
+        val adapter = UnicodeMenuAdapter(menuitems)
+        main_menu_recycler_view.adapter = adapter
+    }
+
+    private fun unicodeItems(): Array<String> { // "", "", "", "",
+        return arrayOf(
+                "☻", "☹",
+                "♡", "♥", "❤", "⚘",
+                "❀", "❃", "❁", "✼",
+                "♫", "♪",
+                "☃", "❄", "❅", "❆",
+                "☕", "☂", "★",
+                "◕‿◕", "｡◕‿◕｡", "｡◕‿‿◕｡", "^̮^",
+                "(◕‿◕)", "(｡◕‿◕｡)", "(｡◕‿‿◕｡)", "(^̮^)",
+                "ʘ‿ʘ", "ಠ_ಠ", "ಠ⌣ಠ", "ಠ‿ಠ",
+                "(ʘ‿ʘ)", "(ಠ_ಠ)", "(ಠ⌣ಠ)", "(ಠ‿ಠ)",
+                "♥‿♥", "◔̯◔", "٩◔̯◔۶", "⊙﹏⊙",
+                " ̳ ̳ ̳ ̳ ͙ ڪ ", "Ƹ̵̡Ӝ̵̨̄Ʒ", "(¬_¬)", "(；一_一)",
+                "(･.◤)", "◕‿↼", "(¬‿¬)", "(づ￣ ³￣)づ",
+                "ب_ب", "(ಥ_ಥ)", "(ಥ﹏ಥ)", "ლ(ಠ益ಠლ)",
+                "ʕ•ᴥ•ʔ", "°Д°", "﴾͡๏̯͡๏﴿ O'RLY?", "(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧",
+                "(ᵔᴥᵔ)", "(•ω•)", "☜(⌒▽⌒)☞", "〆(・∀・＠)",
+                "◔ ⌣ ◔", "ლ(´ڡ`ლ)", "ლ,ᔑ•ﺪ͟͠•ᔐ.ლ", "ᕙ(⇀‸↼‶)ᕗ",
+                "[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]", "(づ｡◕‿‿◕｡)づ", "ᄽὁȍ ̪ őὀᄿ", "▄︻̷̿┻̿═━一",
+                "(╯°□°）╯︵ ┻━┻", "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻", "┬──┬ ノ( ゜-゜ノ)", "(ノಠ益ಠ)ノ彡┻━┻",
+                "（╯°□°）╯︵(\\ .o.)\\", "┬─┬\uFEFF ︵ /(.□. \\）", "╭∩╮(-_-)╭∩╮", "凸(-_-)凸",
+                " ̿ ̿'̿'\\̵͇̿̿\\з=(•_•)=ε/̵͇̿̿/'̿'̿ ̿", "⌐╦╦═─", "(´・ω・)っ由", "（´・ω・ `）",
+                "٩(⁎❛ᴗ❛⁎)۶", "(͡° ͜ʖ ͡°)", "¯\\_(ツ)_/¯", "(° ͜ʖ °)",
+                "¯\\(°_o)/¯", "( ﾟヮﾟ)", "ヽ༼ຈل͜ຈ༽ﾉ", "(︺︹︺)",
+                "┌─┐\n┴─┴\nಠ_ರೃ", "~~~ ╔͎═͓═͙╗\n~~~ ╚̨̈́═̈́﴾ ̥̂˖̫˖̥  ̂ )", "•_•)\n( •_•)>⌐■-■\n(⌐■_■)", "／人 ◕ ‿‿ ◕ 人＼")
+    }
+}
---
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/modules/unicodeKeyboard/recyclerAdapters/UnicodeMenuAdapter.kt
@@ -0,0 +1,77 @@
+package com.itachi1706.cheesecakeutilities.modules.unicodeKeyboard.recyclerAdapters
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Build
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+import com.itachi1706.cheesecakeutilities.R
+
+/**
+ * Created by Kenneth on 11/1/2020.
+ * for com.itachi1706.cheesecakeutilities.modules.unicodeKeyboard.recyclerAdapters in CheesecakeUtilities
+ */
+class UnicodeMenuAdapter(strings: List<String>) : RecyclerView.Adapter<UnicodeMenuAdapter.UnicodeMenuHolder>() {
+
+    private var stringList: List<String> = ArrayList()
+
+    init {
+        stringList = strings
+    }
+
+    constructor(string: Array<String>): this(string.toList())
+
+    override fun getItemCount(): Int {
+        return stringList.size
+    }
+
+    override fun onBindViewHolder(holder: UnicodeMenuHolder, position: Int) {
+        val s = stringList[position]
+        holder.title.text = s
+        holder.title.isSelected = true
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UnicodeMenuHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_default_simple_list_item_1, parent, false)
+        return UnicodeMenuHolder(itemView)
+    }
+
+    inner class UnicodeMenuHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener, View.OnLongClickListener  {
+        var title: TextView = v.findViewById(R.id.text1)
+
+        init {
+            title.ellipsize = TextUtils.TruncateAt.MARQUEE
+            title.marqueeRepeatLimit = -1
+            title.setHorizontallyScrolling(true)
+            v.setOnClickListener(this)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) v.setOnLongClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            val clipboard = v.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = getClip()
+            clipboard.primaryClip = clip
+            Toast.makeText(v.context, "${clip.getItemAt(0).text}\ncopied to clipboard", Toast.LENGTH_LONG).show()
+        }
+
+        @RequiresApi(Build.VERSION_CODES.N)
+        override fun onLongClick(v: View): Boolean {
+            val dragShadowBuilder = View.DragShadowBuilder(v)
+            v.startDragAndDrop(getClip(), dragShadowBuilder, true, View.DRAG_FLAG_GLOBAL or View.DRAG_FLAG_GLOBAL_URI_READ or
+                    View.DRAG_FLAG_GLOBAL_PERSISTABLE_URI_PERMISSION)
+            return true
+        }
+
+        private fun getClip(): ClipData {
+            val emoji = title.text.toString()
+            return ClipData.newPlainText("unicode", emoji)
+        }
+    }
+}
