b6f856797f0be3c483b26f355eca65606523953d
itachi1706 <kennethsohyq@gmail.com>
2020-01-23 23:15:01 +0800

    CUTILAND-161 Implemented some multi select capabilities
    Out of the 3 options in the multi select menu, we have enabled the capability to execute the select all and inverse selections options

--- app/src/main/java/com/itachi1706/cheesecakeutilities/modules/barcodeTools/BarcodeHistoryRecyclerAdapter.kt
+++ app/src/main/java/com/itachi1706/cheesecakeutilities/modules/barcodeTools/BarcodeHistoryRecyclerAdapter.kt
@@ -1,9 +1,13 @@
 package com.itachi1706.cheesecakeutilities.modules.barcodeTools
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.content.Context
+import android.graphics.Color
+import android.util.Log
+import android.view.*
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
 import androidx.recyclerview.widget.RecyclerView
 import com.itachi1706.cheesecakeutilities.R
 import com.itachi1706.cheesecakeutilities.modules.barcodeTools.objects.BarcodeHistory
@@ -30,6 +34,8 @@ class BarcodeHistoryRecyclerAdapter(barcodes: List<BarcodeHistory>) : RecyclerVi
     override fun onBindViewHolder(holder: BarcodeHistoryViewHolder, position: Int) {
         val barcode = barcodeList[position]
         holder.barcode = barcode
+        holder.layout.background = holder.origBackground
+        if (multiSelection.contains(barcode)) holder.layout.setBackgroundColor(Color.LTGRAY)
         if (barcode is BarcodeHistoryGen) {
             // Generated Barcode
             holder.title.text = barcode.text
@@ -46,10 +52,16 @@ class BarcodeHistoryRecyclerAdapter(barcodes: List<BarcodeHistory>) : RecyclerVi
         return BarcodeHistoryViewHolder(itemView)
     }
 
+    // Multiselect handling
+    private var multiMode = false
+    private var multiSelection = ArrayList<BarcodeHistory>()
+
     inner class BarcodeHistoryViewHolder(v: View) : RecyclerView.ViewHolder(v), View.OnClickListener, View.OnLongClickListener {
         var title: TextView = v.findViewById(android.R.id.text1)
         var subtitle: TextView = v.findViewById(android.R.id.text2)
-        var barcode: BarcodeHistory? = null
+        var layout: RelativeLayout = v.findViewById(R.id.layout_two_line)
+        var origBackground = layout.background
+        lateinit var barcode: BarcodeHistory
 
         init {
             v.setOnClickListener(this)
@@ -59,11 +71,84 @@ class BarcodeHistoryRecyclerAdapter(barcodes: List<BarcodeHistory>) : RecyclerVi
 
         override fun onClick(v: View) {
             NotifyUserUtil.createShortToast(v.context, title.text)
+            if (multiMode) {
+                selectItem(barcode, v.context)
+            } else {
+                // TODO: Clicked an object
+            }
         }
 
         override fun onLongClick(v: View): Boolean {
             NotifyUserUtil.createShortToast(v.context, subtitle.text)
+            if (!::barcode.isInitialized) return false
+            if (multiMode) return true // Do not reinit
+            (v.context as AppCompatActivity).startSupportActionMode(actionModeCallback)
+            selectItem(barcode, v.context)
             return true
+        }
+
+        private fun selectItem(item: BarcodeHistory, context: Context) {
+            if (!multiMode) return
+            if (multiSelection.contains(item)) {
+                // Remove
+                multiSelection.remove(item)
+                layout.background = origBackground
+            }
+            else {
+                multiSelection.add(item)
+                layout.setBackgroundColor(Color.LTGRAY)
+            }
+        }
+
+        private val actionModeCallback: ActionMode.Callback = object: ActionMode.Callback {
+            override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+                val dbgSelection = multiSelection.joinToString (",") { s ->
+                    when (s) {
+                        is BarcodeHistoryGen -> s.text
+                        is BarcodeHistoryScan -> s.barcodeValue
+                        else -> "Unknown Barcode"
+                    }
+                }
+                when (item.itemId) {
+                    R.id.menu_select_all -> {
+                        Log.d("BCH", "SELECT ALL: $dbgSelection")
+                        multiSelection.clear()
+                        multiSelection.addAll(barcodeList)
+                        notifyDataSetChanged()
+                    }
+                    R.id.menu_select_invert -> {
+                        Log.d("BCH", "INVERSE: $dbgSelection")
+                        val tmp = ArrayList<BarcodeHistory>()
+                        barcodeList.forEach { if (!multiSelection.contains(it)) tmp.add(it) }
+                        multiSelection.clear()
+                        multiSelection.addAll(tmp)
+                        notifyDataSetChanged()
+                    }
+                    R.id.menu_delete -> {
+                        Log.d("BCH", "DELETE: $dbgSelection")
+                        mode.finish()
+                    }
+                    else -> mode.finish()
+                }
+                return true
+            }
+
+            override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+                multiMode = true
+                mode.menuInflater.inflate(R.menu.context_menu_barcode_hist, menu)
+                return true
+            }
+
+            override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+                return false
+            }
+
+            override fun onDestroyActionMode(mode: ActionMode?) {
+                Log.d("BCH", "Remove")
+                multiMode = false
+                multiSelection.clear()
+                notifyDataSetChanged()
+            }
         }
     }
 }
